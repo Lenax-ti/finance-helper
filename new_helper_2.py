ea25382011d1c53434a89dc1df123abdbf465856
Maksim <sakur0yakyra@gmail.com>
2026-02-21 13:07:17 +0300

    Исправил выход в ГМ. Изменил отображения по запросу "Показать список категорий"

--- new_helper_2.py
+++ new_helper_2.py
@@ -10,12 +10,6 @@ menu = '''Что вы хотите сделать?:
 selection = input(menu)
 
 
-# log_book = {}   # Нужно, что бы данные были сохранены после выполнения кода, а не каждый раз создавались новые
-
-# book = {'market': 1000, 'sport': 500, 'car': 1500, 'cafe': 2500}
-#
-# def recording_expenses():
-#     pass
 
 with open('My expenses.json', 'r') as f:
     log_book = json.load(f)
@@ -32,14 +26,7 @@ while selection:
                 log_book[category] += int(sm)
             else:
                 log_book[category] = int(sm)
-            final_one = input('''Что дальше:
-    1 - Перейти в главное меню
-    2 - Выход
-    ''')
-            if final_one == '1':
-                selection = input(menu)
-            else:
-                break
+            selection = input(menu)
         else:
             print('Сумма, должна быть числом!')
             continue
@@ -55,49 +42,37 @@ while selection:
             select_category = input('Напишите категорию: ')
             if select_category in log_book:
                 print(log_book[select_category])
-                final_two = input('''
-Что дальше:
-1 - Перейти в главное меню
-2 - Выход
-''')
-                if final_two == '1':
-                    selection = input(menu)
-                else:
-                    break
+                selection = input(menu)
+
             else:
                 print('Категория указана не верно!')
                 category_error = input('''
 1 - Повторить ввод
 2 - Показать список категорий
+3 - Выход
 ''')
 
                 if category_error == '2':
-                    print(*log_book.keys())
+                    print(*log_book.keys(), sep='\n')
+                else:
+                    pass
 
+                if category_error == '3':
+                    print('Хорошего дня!')
+                    break
 
-
-        '''
-        Отсюда нужен выход в меню
-        сейчас переходит в раздел "Показать баланс"
-        '''
         if question_about_balance == '2':
-            print(*log_book.values(), sep='\n')
-            final_two = input('''
-Что дальше:
-1 - Перейти в главное меню
-2 - Выход
-''')
-            if final_two == '1':
-                selection = input(menu)
-            else:
-                break
+            for key, value in log_book.items():
+                print(key, value)
+            selection = input(menu)
 
 
     if selection == '3':
+        print('Хорошего дня!')
         break
 
 with open('My expenses.json', 'w') as file:
-    json.dump(log_book, file, indent=21)
+    json.dump(log_book, file, indent=15)
 
 
 
